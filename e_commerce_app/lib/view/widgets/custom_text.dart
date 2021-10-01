38acffaa525bf04d9aa795fb5b401a7f2fd85630
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-01 22:42:01 +0200

    Added new funtion in customtext

--- e_commerce_app/lib/view/widgets/custom_text.dart
+++ e_commerce_app/lib/view/widgets/custom_text.dart
@@ -4,18 +4,22 @@ class CustomText extends StatelessWidget {
   final String text;
   final double fontSize;
   final Color color;
+  final Alignment alignment;
 
-  const CustomText({
-    this.text = '',
-     this.fontSize = 16,
-      this.color = Colors.black
-      });
+  const CustomText(
+      {this.text = '',
+      this.fontSize = 16,
+      this.color = Colors.black,
+      this.alignment = Alignment.topLeft});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(color: color, fontSize: fontSize),
+    return Container(
+      alignment: alignment,
+      child: Text(
+        text,
+        style: TextStyle(color: color, fontSize: fontSize),
+      ),
     );
   }
 }

8b62511c60d9924b2dc79293f7b802b6133e5569
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-01 23:17:17 +0200

    Added SizedBox

--- e_commerce_app/lib/view/auth/login_screen.dart
+++ e_commerce_app/lib/view/auth/login_screen.dart
@@ -42,6 +42,7 @@ class LoginScreen extends StatelessWidget {
               onSaved: (value){},
               validator: (value){},
             ),
+            SizedBox(height:20),
             CustomText(
               text: "Forgot Password?",
               fontSize: 14,

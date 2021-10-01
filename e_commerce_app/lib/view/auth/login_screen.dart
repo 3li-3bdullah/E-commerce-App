b5fa76974b834e0b5aace007a60322f89aa9123d
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-01 21:43:11 +0200

    Added some codes

--- e_commerce_app/lib/view/auth/login_screen.dart
+++ e_commerce_app/lib/view/auth/login_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20 , left:10 , right: 10 ),
+        child: Column(
+          children: [
+            Row(children: [Text("ali"), Text("ali")])
+          ],
+        ),
+      ),
     );
   }
 }

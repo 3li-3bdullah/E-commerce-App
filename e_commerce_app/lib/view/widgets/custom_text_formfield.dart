cbc4265c72d347c1292bebdc71142f9117d1561e
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-01 23:09:14 +0200

    Added some codes

--- e_commerce_app/lib/view/widgets/custom_text_formfield.dart
+++ e_commerce_app/lib/view/widgets/custom_text_formfield.dart
@@ -1,29 +1,36 @@
 import 'package:e_commerce_app/view/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
-
 class CustomTextFormField extends StatelessWidget {
+  final String text;
+  final String hint;
+  final Function onSaved;
+  final Function validator;
 
+  const CustomTextFormField(
+      {this.text, this.hint, this.onSaved, this.validator});
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:Column(children: [
-              CustomText(
-                text: "Email",
-                fontSize: 14,
-                color: Colors.grey.shade900,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "example@gmail.com",
-                  hintStyle: TextStyle(
-                    color: Colors.grey
-                  ),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-              )
-            ],) ,
+      child: Column(
+        children: [
+          CustomText(
+            text: text,
+            fontSize: 14,
+            color: Colors.grey.shade900,
+          ),
+          TextFormField(
+            onSaved: onSaved,
+            validator: validator,
+            decoration: InputDecoration(
+              hintText: hint,
+              hintStyle: TextStyle(color: Colors.grey),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+          )
+        ],
+      ),
     );
   }
 }

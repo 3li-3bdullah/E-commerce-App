dee164c812e57885a7d7e3974e0e62a79811a747
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-02 23:56:32 +0200

    Added 2 parameter

--- e_commerce_app/lib/view/widgets/custom_button_social.dart
+++ e_commerce_app/lib/view/widgets/custom_button_social.dart
@@ -2,21 +2,24 @@ import 'package:e_commerce_app/view/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class CustomButtonSocial extends StatelessWidget {
+  final String text;
+  final String imageName;
+
+  const CustomButtonSocial({this.text, this.imageName});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:Material(
-              borderRadius: BorderRadius.circular(10),
-              child: MaterialButton(
-                onPressed: () {},
-                child: Row(children: [
-                  Image.asset('assets/images/google.png'),
-                  SizedBox(width: 90),
-                  CustomText(text: "Sign In with Google")
-                ]),
-              ),
-            )
-    );
+        child: Material(
+      borderRadius: BorderRadius.circular(10),
+      child: MaterialButton(
+        onPressed: () {},
+        child: Row(children: [
+          Image.asset(imageName),
+          SizedBox(width: 90),
+          CustomText(text: text)
+        ]),
+      ),
+    ));
   }
 }

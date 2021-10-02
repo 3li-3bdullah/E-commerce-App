ba3cc80103e15882817852dab319566b7ee69e51
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-03 00:04:44 +0200

    Added some parameter

--- e_commerce_app/lib/view/widgets/custom_button_social.dart
+++ e_commerce_app/lib/view/widgets/custom_button_social.dart
@@ -4,22 +4,26 @@ import 'package:flutter/material.dart';
 class CustomButtonSocial extends StatelessWidget {
   final String text;
   final String imageName;
+  final Function onPress;
 
-  const CustomButtonSocial({this.text, this.imageName});
+  const CustomButtonSocial({this.text, this.imageName, this.onPress});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.grey.shade100),
         child: Material(
-      borderRadius: BorderRadius.circular(10),
-      child: MaterialButton(
-        onPressed: () {},
-        child: Row(children: [
-          Image.asset(imageName),
-          SizedBox(width: 90),
-          CustomText(text: text)
-        ]),
-      ),
-    ));
+          borderRadius: BorderRadius.circular(10),
+          child: MaterialButton(
+            onPressed: onPress,
+            child: Row(children: [
+              Image.asset(imageName),
+              SizedBox(width: 90),
+              CustomText(text: text)
+            ]),
+          ),
+        ));
   }
 }

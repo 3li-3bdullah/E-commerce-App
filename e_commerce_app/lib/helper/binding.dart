ef6cbf0b937abe5483935897de62264e3c03fe24
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-04 23:17:06 +0200

    Added some features

--- e_commerce_app/lib/helper/binding.dart
+++ e_commerce_app/lib/helper/binding.dart
@@ -1,10 +1,9 @@
+import 'package:e_commerce_app/core/services/view_modal/auth_view_model.dart';
 import 'package:get/get.dart';
 
-
-class Binding extends Bindings{
+class Binding extends Bindings {
   @override
   void dependencies() {
-   
+    Get.lazyPut(() => AuthViewModel());
   }
-
 }
